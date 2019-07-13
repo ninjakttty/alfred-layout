ff1d9497b014974c01838f67533de57c38e25041
Yuri Parsons <yuri@ninjakitty.net>
2019-07-13 10:54:35 -0700

    add icons and layouts

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-'use strict';
-const alfy = require('alfy');
+const alfy = require('alfy')
+const layouts = require('./layouts.json')
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const results = alfy.inputMatches(layouts, 'name').map((item) => ({
+  title: item.name,
+  arg: item.key,
+  uid: item.name,
+  subtitle: item.display,
+  autocomplete: item.display,
+  icon: {
+    path: `./icons/${item.name}.png`,
+  },
+}))
+
+alfy.output(results)
